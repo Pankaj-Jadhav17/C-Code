c3bec5bbc5fef2c3601c2e746543d7faef7e82e8
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-08-03 01:11:35 +0530

    Initial commit

---
+++ p2.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int i=0;
+printf("enter a number\n");
+scanf("%d",&i);
+printf("the number is : %d\n",i);
+return 0;
+}
+
+/*
+#include<stdio.h>
+int main()
+{
+int number;
+printf("enter a number");
+scanf("%d",&number);
+printf("you entered number : %d\n",number);
+return 0;
+}
+*/
