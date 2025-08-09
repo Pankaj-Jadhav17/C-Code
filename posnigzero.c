2fec2e9cef56edb7b01db28acfda29514afdbc29
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-08-09 10:17:36 +0530

    display pos neg & zero nos

---
+++ posnigzero.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number...");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("number is positive\n");
+    }
+    else if(n<0)
+        printf("number is negative\n");
+        else
+        printf("number is zero\n");
+}
