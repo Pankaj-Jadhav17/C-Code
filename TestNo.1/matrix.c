b5d47835009604a0cf342b557a23d8186005d433
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-09-08 10:30:29 +0530

    test questions

---
+++ TestNo.1/matrix.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int n, m;
+    int count = 1;
+    
+    printf("Enter number of rows: ");
+    scanf("%d", &n);
+
+    printf("Enter number of columns: ");
+    scanf("%d", &m);
+
+    printf("Number\tSquare\n");
+
+    int row = 0;
+    while (row < n) {
+        for (int col = 0; col < m; col++) {
+            printf("%d\t%d\n", count, count * count);
+            count++;
+        }
+        row++;
+    }
+
+    return 0;
+}
