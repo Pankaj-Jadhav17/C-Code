2aae176797bf4ef6edc856f7fd229af206bd3524
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-09-10 22:44:13 +0530

    square of x

---
+++ Arithmatic_and_Expression_Evaluation/arithmatic_1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int x=5;
+    printf("x= %d\n",x*x);
+}
+
+
+/*
+
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter a number:");
+    scanf("%d",&x)
+    printf("x= %d\n",x*x);
+}
+    
+*/
