7d37277eac8bd0f8a4f9feaf6ed196a208baebc4
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-09-13 22:05:46 +0530

    for loop

---
+++ Nested_For_Loop/nestedfor_2.c
@@ -0,0 +1,29 @@
+//number is prime or not
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int n=0;
+    int i,j,flag;
+    printf("enter a number..");
+    scanf("%d",&n);
+    int st=sqrt(i);
+    for(i=0;i<=n;i++)
+    {
+        int flag=0;
+        sqrt-(i);
+        for(int j=2;j<i;j++)
+        {
+            if(i%j==0)
+            {
+                flag=1;
+                break;
+            }
+        }
+        if(flag == 0)
+        printf("%d is not prime\n",i);
+        else
+        printf("%d is prime\n",i);
+
+    }
+}
