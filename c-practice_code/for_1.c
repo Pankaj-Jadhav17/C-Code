4e7e7468d49ab1a5978827b91360bb6b785be289
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-09-14 21:09:40 +0530

    practice code

---
+++ c-practice_code/for_1.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=0;i<5;i++)
+    {
+        printf("%d\n",i);
+    }
+   return 0;
+}
+
+
+
+
+/*
+#include<stdio.h>
+int main()
+{
+    for(int i=10;i>=0;i=i-1)
+    {
+        printf("%d \n",i);
+    }
+    return 0;
+}
+     */
---
+++ c-practice_code/for_IDorder.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    //increament order
+    //++i(pre-increament) i++(post-increament)
+    int i=1;
+    //printf("%d\n",i++);  //use then increase
+    //return 0;
+
+    //--i(pre-decreament)
+    //i--(post-decreament)
+
+    //printf("%d\n",i--);
+    //printf("%d\n",i);
+
+    printf("%d\n",--i);
+    printf("%d\n",i);
+
+}
