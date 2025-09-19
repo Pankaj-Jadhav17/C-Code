e363dee1de65910c632d353a4e6ba86fbe3155ea
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-09-20 05:10:52 +0530

    number is palindrome or not

--- ARRAY_BASICS/array_5.c
+++ ARRAY_BASICS/array_5.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,a[n];
+    int n,a[n];
     printf("Enter number of entegers: ");
     scanf("%d",&n);
-    printf("enter %d elements",i);
-    for(i=0;i<n;i++)
+    printf("enter %d elements\n",n);
+    for(int i=0;i<n;i++)
     {
     scanf("%d",&a[i]);
     }
@@ -31,43 +31,3 @@ int main()
 }
 
 
-/*
-#include <stdio.h>
-
-int main()
-{
-    int n;
-    printf("Enter number of integers: ");
-    scanf("%d", &n);
-
-    int a[n]; // VLA — okay in C99 or later
-
-    printf("Enter %d integers:\n", n);
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", &a[i]);
-    }
-
-    int i = 0;
-    int j = n - 1;
-    int isPalindrome = 1; // Assume true
-
-    while (i < j)
-    {
-        if (a[i] != a[j])
-        {
-            isPalindrome = 0; // Found a mismatch
-            break;
-        }
-        i++;
-        j--;
-    }
-
-    if (isPalindrome)
-        printf("The number is a palindrome.\n");
-    else
-        printf("The number is not a palindrome.\n");
-
-    return 0;
-}
-*/
