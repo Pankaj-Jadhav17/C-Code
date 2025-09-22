b0c622805be7a93f8d7127450ca498fd1a7fdef6
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-09-23 05:06:03 +0530

    sum of square of first n nos

---
+++ For_Loop/for_loop_7.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int i=1,n;
+    printf("enter a number: ");
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        printf("%d*%d=%d\n",i,i,i*i);
+    }
+    printf("\n");
+    return 0;
+}
