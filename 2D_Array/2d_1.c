47f13e8cfc4b19db5657cf9696801aaf490c5db0
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-10-08 14:55:54 +0530

    Using 2d array

---
+++ 2D_Array/2d_1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+int main()
+{
+    int n=3;
+    int a[3][3]={
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+    };
+     int row=0,col=0;
+    for(row=0;row<n;row++)
+    {
+        for(col=0;col<n;col++)
+        {
+            printf("%d ",a[row][col]);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+
+
+
+
+// #include <stdio.h>
+// int main() {
+//     int matrix[3][3] = { {1, 2, 3},
+//                          {4, 5, 6},
+//                          {7, 8, 9} };
+//     for(int i = 0; i < 3; i++) 
+//     {
+//         for(int j = 0; j < 3; j++) 
+//         {
+//             printf("%d ", matrix[i][j]);
+//         }
+//         printf("\n");
+//     }
+//     return 0;
+// }
