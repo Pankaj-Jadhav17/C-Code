2fff834888cf2bed0ff6828ba0bfd96eeeba3b50
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2025-10-30 13:12:23 +0530

    Matrix code

---
+++ LAB/lab1/matrix2.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    printf("Enter (n*n) matrix: ");
+    scanf("%d",&n);
+   
+    int matrixA[n][n];
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n; i++)
+        {
+            scanf("%d",&matrixA[i][j]);
+        }
+    }
+    printf("\n");
+
+    int matrixB[n][n];
+    for ( i = 0; i < n; i++)
+    {
+        for ( j = 0; j < n; j++)
+        {
+            scanf("%d",&matrixB[i][j]);
+        }
+    }
+    printf("\n");
+    
+    
+    int matrixC[i][j];
+    printf("addition of whole matrix: ");
+     for ( i = 0; i < n; i++ ){
+        for ( i = 0; i < n; i++){
+           matrixC[i][j]= matrixA[i][j]+matrixB[i][j];
+        }
+    }
+    printf("\n");
+    
+}
+
+
+
+//int val=0 , ans=0;
