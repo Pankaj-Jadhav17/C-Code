dd391981fb9fdd190ec3fcb16b1e18803026e879
Pankaj Jadhav <pankajjadhav1712@gmail.com>
2025-11-06 17:11:05 +0530

    Add files via upload

---
+++ Home_Assignment/Section_1/S1_V.c
@@ -0,0 +1,11 @@
+// Get and display the size of int, float, double, and char using sizeof
+
+#include<stdio.h>
+int main()
+{
+    printf("The size of integer = %zu bytes\n",sizeof(int));
+    printf("The size of float = %zu bytes\n",sizeof(float));
+    printf("The size of double = %zu bytes\n",sizeof(double));
+    printf("The size of character = %zu bytes\n",sizeof(char));
+   return 0;
+}
