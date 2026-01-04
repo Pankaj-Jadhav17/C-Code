a113279bfd5a2091d9e2f88787da9c58c4c7a830
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-01-04 11:07:54 +0530

    array soted by ascending order

--- PPL1/Insertionsort.c
+++ PPL1/Insertionsort.c
@@ -1,30 +1,36 @@
-#include<stdio.h>
+#include <stdio.h>
 void insertionsort(int a[], int n)
 {
-    int i,j,key;
-    for ( j = 1; j < n; j++)
+    int i, j, key;
+    for (j = 1; j < n; j++)
     {
-        key=a[i];
-        for ( i = j-1; i >= 0; i--)
+        key = a[j];     
+        i = j - 1;
+
+        while (i >= 0 && a[i] > key)   
         {
-            if (a[i]>key)
-                a[i+1]=a[i];
-                else
-                break;
+            a[i + 1] = a[i];
+            i--;
         }
-        a[i+1]=key;
+        a[i + 1] = key;
     }
 }
 int main()
 {
-    int i,n,a[100];
+    int i, n, a[100];
+
     printf("Enter how many elements: ");
-    scanf("%d",&n);
-    printf("Enter %d elements: \n");
-    for(i=0;i<n;i++)
-    scanf("%d",&a[i]);
-    insertionsort(a,n);
-    printf("\n The element in sorte order are..\n");
-    for ( i = 0; i < n; i++)
-    printf("%d",a[i]);
+    scanf("%d", &n);
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    insertionsort(a, n);
+
+    printf("\nThe elements in sorted order are:\n");
+    for (i = 0; i < n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
 }
