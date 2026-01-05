09c1026ae369c504f9ef981d13e1af77ebde5bb0
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-01-05 09:33:25 +0530

    element search in array

--- PPL1/linearSearch.c
+++ PPL1/linearSearch.c
@@ -1,21 +1,56 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
-    int i,n,a[n];
-    printf("Enter How many elements: ");
-    scanf("%d",&n);
-    for ( i = 0; i<n; i++)
-    scanf("%d",&a[i]);
-    for ( i = 0; i < n; i++)
+    int n, i, key;
+
+    printf("Enter how many elements: ");
+    scanf("%d", &n);
+
+    int a[n];   // declare array AFTER reading n
+
+    printf("Enter %d elements:\n", n);
+    for (i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    for (i = 0; i < n; i++)
     {
+        if (a[i] == key)
         {
-        if(a[i]==n)
-        return i;
+            printf("Element found at index %d\n", i);
+            return 0;
         }
-    return -1;
     }
-    for ( i = 0; i < n; i++)
-    {
-        printf("%d",a[i]);
-    }
+
+    printf("Element not found\n");
+    return 0;
 }
+
+
+
+
+
+// #include<stdio.h>
+// int main()
+// {
+//     int i,n,a[n];
+//     printf("Enter How many elements: ");
+//     scanf("%d",&n);
+//     for ( i = 0; i<n; i++)
+//     scanf("%d",&a[i]);
+//     for ( i = 0; i < n; i++)
+//     {
+//         {
+//         if(a[i]==n)
+//         return i;
+//         }
+//     return -1;
+//     }
+//     for ( i = 0; i < n; i++)
+//     {
+//         printf("%d",a[i]);
+//     }
+// }
