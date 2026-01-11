9bdf47829d8b1fa4e9199621a28c84b7dbb2faf0
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-01-11 10:59:45 +0530

    find the missing number

---
+++ leet_code/SetMismatch.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+int* MissNums(int* nums, int n, int* returnSize) {
+    int dup = -1, miss = -1;
+    for (int i = 0; i < n; i++) {
+        int idx = abs(nums[i]) - 1;
+        if (nums[idx] < 0)
+            dup = abs(nums[i]);
+        else
+            nums[idx] = -nums[idx];
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (nums[i] > 0) {
+            miss = i + 1;
+            break;
+        }
+    }
+    int* ans = (int*)malloc(2 * sizeof(int));
+    ans[0] = dup;
+    ans[1] = miss;
+
+    *returnSize = 2;
+    return ans;
+}
+int main() {
+    int nums[] = {1, 2, 2, 4};
+    int returnSize;
+    int* ans = MissNums(nums, 4, &returnSize);
+    printf("Duplicate: %d\n", ans[0]);
+    printf("Missing: %d\n", ans[1]);
+    free(ans);
+    return 0;
+}
+
+
