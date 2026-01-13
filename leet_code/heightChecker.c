58426feea2224a00ed871e19533123964f3cafa4
Pankaj-Jadhav17 <pankajjadhav1712@gmail.com>
2026-01-13 09:42:05 +0530

    count number of student in incorrect position

--- leet_code/heightChecker.c
+++ leet_code/heightChecker.c
@@ -1,19 +1,3 @@
-// class Solution {
-// public:
-//     int heightChecker(vector<int>& heights) {
-//         int sz = heights.size();
-//         vector<int> e(heights);
-//         sort(e.begin(), e.end());
-//         int cnt = 0;
-//         for(int i=0;i<sz;i++){
-//             if(heights[i] != e[i]){
-//                 cnt++;
-//             }
-//         }
-//         return cnt;
-//     }
-// };
-
 #include <stdio.h>
 #include <stdlib.h>
 
